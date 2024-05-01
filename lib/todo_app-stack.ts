15cadf5794dfa0aee08106da4d4f7e4c40bdd266
Charu Srivastava <charusri97@gmail.com>
2024-05-01 13:12:21 +0900

    Added dynamodb table

--- lib/todo_app-stack.ts
+++ lib/todo_app-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {aws_lambda,
+        aws_dynamodb} from 'aws-cdk-lib'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TodoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const toDoLambda = new cdk.aws_lambda_nodejs.NodejsFunction(this, "ToDoLambda",{
+      runtime: aws_lambda.Runtime.NODEJS_20_X,
+      architecture: aws_lambda.Architecture.ARM_64,
+      entry: "lib/src/todoFunc/index.ts"
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TodoAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const toDoTable = new cdk.aws_dynamodb.Table(this, "ToDoTable",{
+      partitionKey: {
+        name: "task_num",
+        type: aws_dynamodb.AttributeType.NUMBER
+      }
+    })
+    
   }
 }
