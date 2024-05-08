723f5aa260159bbb9db913a34a698dff16a0f7d5
Charu Srivastava <charusri97@gmail.com>
2024-05-08 14:18:17 +0900

    Applied biome lint

--- packages/cdk/lib/todo_app-stack.ts
+++ packages/cdk/lib/todo_app-stack.ts
@@ -1,28 +1,29 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import {aws_lambda,
-        aws_dynamodb,
-        } from 'aws-cdk-lib'
+import * as cdk from "aws-cdk-lib";
+import { aws_dynamodb, aws_lambda } from "aws-cdk-lib";
+import type { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TodoAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    const toDoLambda = new cdk.aws_lambda_nodejs.NodejsFunction(this, "ToDoLambda",{
-      runtime: aws_lambda.Runtime.NODEJS_20_X,
-      architecture: aws_lambda.Architecture.ARM_64,
-      entry: "../lambda/src/todoFunc/index.ts"
-    })
+		const toDoLambda = new cdk.aws_lambda_nodejs.NodejsFunction(
+			this,
+			"ToDoLambda",
+			{
+				runtime: aws_lambda.Runtime.NODEJS_20_X,
+				architecture: aws_lambda.Architecture.ARM_64,
+				entry: "../lambda/src/todoFunc/index.ts",
+			},
+		);
 
-    const toDoTable = new cdk.aws_dynamodb.Table(this, "ToDoTable",{
-      partitionKey: {
-        name: "task_num",
-        type: aws_dynamodb.AttributeType.NUMBER
-      }
-    })
+		const toDoTable = new cdk.aws_dynamodb.Table(this, "ToDoTable", {
+			partitionKey: {
+				name: "task_num",
+				type: aws_dynamodb.AttributeType.NUMBER,
+			},
+		});
 
-    // const toDoAPI = new cdk.aws_apigateway.RestApi(this, "ToDoAPI")
-    
-  }
+		// const toDoAPI = new cdk.aws_apigateway.RestApi(this, "ToDoAPI")
+	}
 }
--- packages/lambda/src/todoFunc/index.ts
+++ packages/lambda/src/todoFunc/index.ts
@@ -1,4 +1,4 @@
-export const handler = async() => {
-    console.log("Hello World!")
-    return {message: "Hello"}
-}
+export const handler = async () => {
+	console.log("Hello World!");
+	return { message: "Hello" };
+};
